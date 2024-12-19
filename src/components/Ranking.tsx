d22409e1fb3c6c099b5e5ceca65d196b34d7dc3e
sirWerq <rifqx1904@gmail.com>
2024-12-19 21:01:24 +0700

    fix some issues

--- src/components/Ranking.tsx
+++ src/components/Ranking.tsx
@@ -1,65 +1,70 @@
 function RankingComponent({ motion }) {
     return (
-        <motion.div
-            initial={{ opacity: 0, translateX: -100 }}
-            whileInView={{
-                opacity: 1,
-                translateX: 0,
-            }}
-            transition={{ duration: 0.5 }}
-            className='vh-100 text-dark d-flex flex-column align-items-center gap-4'
+        <div
+            className='vh-100 text-dark'
             style={{ backgroundColor: '#EDF5E1', paddingTop: '60px' }}
         >
-            <h2 className='mb-4'>Ranking</h2>
-            <div
-                className='card'
-                style={{
-                    width: '70vw',
+            <motion.div
+                initial={{ opacity: 0, translateX: -100 }}
+                whileInView={{
+                    opacity: 1,
+                    translateX: 0,
                 }}
+                transition={{ duration: 0.5 }}
+                className='d-flex flex-column align-items-center gap-4'
+                style={{ backgroundColor: '#EDF5E1', paddingTop: '60px' }}
             >
-                <div className='card-body'>Akiyama Mizuki</div>
-            </div>
-            <div
-                className='card'
-                style={{
-                    width: '70vw',
-                }}
-            >
-                <div className='card-body'>Oumae Kumiko</div>
-            </div>
-            <div
-                className='card'
-                style={{
-                    width: '70vw',
-                }}
-            >
-                <div className='card-body'>Yoroizuka Mizore</div>
-            </div>
-            <div
-                className='card'
-                style={{
-                    width: '70vw',
-                }}
-            >
-                <div className='card-body'>Kurokawa Akane</div>
-            </div>
-            <div
-                className='card'
-                style={{
-                    width: '70vw',
-                }}
-            >
-                <div className='card-body'>Tokisaki Kurumi</div>
-            </div>
-            <div
-                className='card'
-                style={{
-                    width: '70vw',
-                }}
-            >
-                <div className='card-body'>Touma Kazusa</div>
-            </div>
-        </motion.div>
+                <h2 className='mb-4'>Ranking</h2>
+                <div
+                    className='card'
+                    style={{
+                        width: '70vw',
+                    }}
+                >
+                    <div className='card-body'>Akiyama Mizuki</div>
+                </div>
+                <div
+                    className='card'
+                    style={{
+                        width: '70vw',
+                    }}
+                >
+                    <div className='card-body'>Oumae Kumiko</div>
+                </div>
+                <div
+                    className='card'
+                    style={{
+                        width: '70vw',
+                    }}
+                >
+                    <div className='card-body'>Yoroizuka Mizore</div>
+                </div>
+                <div
+                    className='card'
+                    style={{
+                        width: '70vw',
+                    }}
+                >
+                    <div className='card-body'>Kurokawa Akane</div>
+                </div>
+                <div
+                    className='card'
+                    style={{
+                        width: '70vw',
+                    }}
+                >
+                    <div className='card-body'>Tokisaki Kurumi</div>
+                </div>
+                <div
+                    className='card'
+                    style={{
+                        width: '70vw',
+                    }}
+                >
+                    <div className='card-body'>Touma Kazusa</div>
+                </div>
+            </motion.div>
+        </div>
     );
 }
 
